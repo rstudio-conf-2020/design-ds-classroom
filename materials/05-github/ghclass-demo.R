b311c688d2a19985c8844a0095a866a7d09ed5af
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-01-28 03:44:47 -0800

    Clean up folder and add parting thoughts

--- materials/05-github/ghclass-demo.R
+++ materials/05-github/ghclass-demo.R
@@ -1,74 +1,52 @@
 # load packages ----------------------------------------------------------------
 
-library(devtools)
-#install_github("rundel/ghclass")
 library(ghclass)
 library(tidyverse)
 
-# clean up ---------------------------------------------------------------------
+# load data --------------------------------------------------------------------
 
-repo_delete(org_repos("ghclass-demo",
-                      filter = "hw-01-"))
-
-
-
-# load roster data -------------------------------------------------------------
-
-roster <- read_csv("03-reproducible-workflows/Teaching Data Science - GitHub names (Responses) - Form Responses 1.csv")
-
-roster <- roster %>%
-  slice(c(1,37:nrow(roster)))
-
-names(roster) <- c("timestamp", "email", "name", "ghname")
-
-# define org -------------------------------------------------------------------
-
-org = "ghclass-demo"
+roster <- read_csv("materials/05-github/GitHub Usernames - Designing the Data Science Classroom (Responses) - Form Responses 1.csv")
+org = "design-ds-class"
 
 # invite users -----------------------------------------------------------------
 
-org_invite(org = org, user = roster$ghname)
+org_invite(org = org, user = roster$username)
 
-org_invite(org = org, user = "ricardomayerb")
+# member status ----------------------------------------------------------------
+
+org_members(org)
+org_pending(org)
 
 # create individual assignment -------------------------------------------------
 
-org_create_assignment(org = org, 
-                      repo = paste0("hw-01-", roster$ghname), 
-                      user = roster$ghname, 
-                      source_repo = "ghclass-demo/hw-01")
-
-# create teams -----------------------------------------------------------------
-
-team_names <- c("team1", "team2", "team3", "team4", "team5", "team6", "team7", "team8", "team9", "team10")
-
-roster <- roster %>%
-  mutate(team = sample(team_names, nrow(roster), replace = TRUE))
-
-team_create(org = org,
-            team = unique(roster$team))
-
-team_invite(org = org,
-            user = roster$ghname,
-            team = roster$team)
-
-# create team assignment -------------------------------------------------
-
-org_create_assignment(org = org, 
-                      repo = paste0("hw-02-", roster$team), 
-                      user = roster$ghname, 
-                      team = roster$team,
-                      source_repo = "ghclass-demo/hw-02")
+org_create_assignment(
+  org = org,
+  repo = paste0("hw-01-airbnb-edi-", roster$username),
+  user = roster$username,
+  source_repo = paste0(org, "/hw-01-airbnb-edi")
+)
 
 # collect assignments ----------------------------------------------------------
 
-hw01_repos <- org_repos(org = org, filter = "hw-01-")
+hw01_repos <- org_repos(org, "hw-01-airbnb-edi-")
+local_repo_clone(hw01_repos, local_path = "hw01_submissions")
 
-local_repo_clone(repo = hw01_repos, 
-                 local_path = "hw-01-collect")
+# create issues ----------------------------------------------------------------
 
-# styling ----------------------------------------------------------------------
+issue_create(roster$hw01_repo, # should be in org/repo format
+             title = "Feedback",
+             body = roster$hw01_feedback)
 
-repo_style(repo = "ghclass-demo/hw-01-minebotmine", 
-           files = "*.Rmd", draft = TRUE)
+# view repo stats --------------------------------------------------------------
 
+org_repo_stats(org, filter = "hw-01-airbnb-edi-")
+
+# create team assignments ------------------------------------------------------
+
+org_create_assignment(
+  org = org,
+  repo = paste0("lab-03-nobel-laureates-", roster$team_name),
+  user = roster$github_name,
+  team = roster$team_name,
+  source_repo = paste0(org, "/lab-03-nobel-laureates")
+)
